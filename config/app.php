759a5fd6a4220705b419d9edcf4f75400f48c634
Glenn Michael Torregosa <gtorregosa@gmail.com>
2020-06-18 10:52:45 +0000

    config file for supported locales

--- config/app.php
+++ config/app.php
@@ -112,6 +112,19 @@ return [
 
     /*
     |--------------------------------------------------------------------------
+    | Supported Locales
+    |--------------------------------------------------------------------------
+    |
+    | These are the list of currently supported locales
+    | If the user sets a locale not set in the array it will fall back
+    | to the user default in the users table
+    |
+    */
+
+    'supported_locales' => ['nl', 'fr', 'en'],
+
+    /*
+    |--------------------------------------------------------------------------
     | Encryption Key
     |--------------------------------------------------------------------------
     |
